29330f7fd17e60f3945ac2408792042ddf70cbd7
YYun-D <yeo1998@naver.com>
2024-03-24 19:27:47 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -78,25 +78,30 @@ int main(int argc, char* argv[]) {
 
 	char *macAddress = getMacAddress(dev);
 
-	EthArpPacket packet;
+	for (int i=2; i<argc; i+=2){
+		char *sender_ip = argv[i];
+		char *target_ip = argv[i+1];
+		
+		EthArpPacket packet;
 
-	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.type_ = htons(EthHdr::Arp);
+		packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
+		packet.eth_.smac_ = Mac(macAddress);
+		packet.eth_.type_ = htons(EthHdr::Arp);
 
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
+		packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+		packet.arp_.pro_ = htons(EthHdr::Ip4);
+		packet.arp_.hln_ = Mac::SIZE;
+		packet.arp_.pln_ = Ip::SIZE;
+		packet.arp_.op_ = htons(ArpHdr::Request);
+		packet.arp_.smac_ = Mac(macAddress);
+		packet.arp_.sip_ = htonl(Ip(target_ip));
+		packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
+		packet.arp_.tip_ = htonl(Ip(sender_ip));
 
-	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+		int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+		if (res != 0) {
+			fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+		}
 	}
 
 	pcap_close(handle);
